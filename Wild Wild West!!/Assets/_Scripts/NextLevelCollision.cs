a5ce6fc7dd35af77bc37050664e3e57b1f6b1db4
Liam <90796483+lyoell@users.noreply.github.com>
2022-04-04 17:00:24 -0400

    Added a way to get the to the next level after the saloon

---
+++ Wild Wild West!!/Assets/_Scripts/NextLevelCollision.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextLevelCollision : MonoBehaviour
+{
+    public GameObject plate;
+    GameChange game;
+    public GameObject player;
+
+    public void Update()
+    {
+        if (Mathf.Abs(plate.transform.position.magnitude - player.transform.position.magnitude) <1f)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       
+    }
+}
+    
+
