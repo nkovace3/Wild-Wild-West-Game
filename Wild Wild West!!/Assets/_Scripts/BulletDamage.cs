c63055d8904e052cbaf0dea16a3ea0252cabbb19
Liam <90796483+lyoell@users.noreply.github.com>
2022-04-04 21:34:32 -0400

    Added more characters (NPC)

---
+++ Wild Wild West!!/Assets/_Scripts/BulletDamage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BulletDamage : MonoBehaviour
+{
+    public GameObject player;
+    public int health = 100;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Bullet"))
+        {
+            health -= 100;
+        }
+    }
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            player.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+           
+        }
+    }
+}
