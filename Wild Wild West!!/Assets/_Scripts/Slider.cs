110acb4758e90157a6d9064130cb865590bd3a99
Brayden Thompson <bthomp89@uwo.ca>
2022-04-05 20:51:34 -0400

    added sliding NPC on train

--- Wild Wild West!!/Assets/_Scripts/Slider.cs
+++ Wild Wild West!!/Assets/_Scripts/Slider.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Slider : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 0.4f;
+    Vector3 pointA;
+    Vector3 pointB;
+
     void Start()
     {
-        
+        pointA = new Vector3(-47.93f, -128.8553f, 170.8432f);
+        pointB = new Vector3(-35.31f, -128.8553f, 170.8432f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //PingPong between 0 and 1
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
     }
 }
